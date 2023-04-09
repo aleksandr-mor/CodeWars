c8657307b6f3c951884a8546e43de341bb968d0e
Aleksandr Morozov <dr.morozov@hotmail.com>
2023-04-09 01:13:32 -0700

    Solved

---
+++ Simple multiplication.playground/Contents.swift
@@ -0,0 +1,7 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+func simpleMultiplication(_ num: Int) -> Int {
+    num % 2 == 0 ? num * 8 : num * 9
+}
+
+// Implicit return with ternary operator
---
+++ Switch it Up!.playground/Contents.swift
@@ -0,0 +1,40 @@
+//When provided with a number between 0-9, return it in words.
+//Input: 1
+//Output: "One".
+//If your language supports it, try using a switch statement.
+
+
+// Using a switch statement
+
+func switchItUp(_ number: Int) -> String {
+    switch number {
+    case 0:
+        return "Zero"
+    case 1:
+        return "One"
+    case 2:
+        return "Two"
+    case 3:
+        return "Three"
+    case 4:
+        return "Four"
+    case 5:
+        return "Five"
+    case 6:
+        return "Six"
+    case 7:
+        return "Seven"
+    case 8:
+        return "Eight"
+    case 9:
+        return "Nine"
+    default:
+        return ""
+    }
+}
+
+// Not using a switch statement
+
+func switchItUp(_ number: Int) -> String {
+  return ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][number]
+}
