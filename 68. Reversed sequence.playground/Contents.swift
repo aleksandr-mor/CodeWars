c4f9d617b30603d7833a8e24dd7b67041cb0a896
Aleksandr Morozov <dr.morozov@hotmail.com>
2023-05-20 21:34:17 -0700

    Solved

---
+++ 68. Reversed sequence.playground/Contents.swift
@@ -0,0 +1,8 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+//
+// Example : n=5 --> [5,4,3,2,1]
+
+func reverseSeq(n: Int) -> [Int] {
+    (1...n).reversed()
+}
+reverseSeq(n: 5)
